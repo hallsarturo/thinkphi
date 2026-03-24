f336ce83903f09c4694d90a446cf571781321872
hallsarturo <proalarturo@gmail.com>
2026-03-23 18:35:07 -0600

    refactor: remove unused signIn email test code from auth-client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,4 @@
 import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient();
-
-await authClient.signIn.email({
-    email: 'test@user.com',
-    password: 'password1234',
-});
-
+ 
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
