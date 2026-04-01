8bfae3c563dec46c791e8be2f90e2318337d97c1
hallsarturo <proalarturo@gmail.com>
2026-04-01 16:30:24 -0600

    feat: enhance Page component layout with improved styling and structure

--- app/topics/page.tsx
+++ app/topics/page.tsx
@@ -1,5 +1,13 @@
 export default function Page() {
     return (
-        <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8"></div>
+        <div className="min-h-screen overflow-hidden bg-white py-12 sm:py-6 dark:bg-gray-900">
+            <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+                <div className="border-b border-gray-200 pb-5 dark:border-white/10">
+                    <h3 className="text-base font-semibold text-gray-900 dark:text-white">
+                        Topics
+                    </h3>
+                </div>
+            </div>
+        </div>
     );
 }
