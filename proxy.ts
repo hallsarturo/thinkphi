4835002ac520e67a28ec70fa6f5858eb9fa352bf
hallsarturo <proalarturo@gmail.com>
2026-04-04 12:07:43 -0600

    feat(proxy): implement middleware for session validation on protected paths and add PaymentPage component

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,31 @@
+import { NextRequest, NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+// Localization routing Next-Intl
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(request: NextRequest) {
+    // Run Next-Intl middleware
+    const response = intlMiddleware(request);
+
+    // Optimistic secure proxing (better-auth, next.js)
+
+    // Protected paths
+    const protectedPaths = ['/payment'];
+    const { pathname } = request.nextUrl;
+
+    if (protectedPaths.some((path) => pathname.startsWith(path))) {
+        // Check for the session cookie
+        const session = request.cookies.get('better-auth.session_token');
+        if (!session) {
+            return NextResponse.redirect(new URL('/sign-up', request.url));
+        }
+    }
+
+    // Continue
+    return response;
+}
 
 export const config = {
     // Match all pathnames except for
