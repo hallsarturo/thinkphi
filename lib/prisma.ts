c299682d63d9a09b4c729357bbf808389377edc3
hallsarturo <proalarturo@gmail.com>
2026-04-11 17:41:23 -0600

    refactor(prisma): remove PrismaPg adapter and streamline PrismaClient initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,16 @@
 import 'dotenv/config';
 import { PrismaClient } from '@/lib/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL!,
-});
-
 const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
-        adapter,
-        log: ['query', 'info', 'warn', 'error']
-    });
+        accelerateUrl: process.env.DATABASE_URL!,
+    }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
