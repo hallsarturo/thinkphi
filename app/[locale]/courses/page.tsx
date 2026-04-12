a8584a217b1c904ac06ebd0e72060a43211f3f7a
hallsarturo <proalarturo@gmail.com>
2026-04-12 10:06:48 -0600

    feat(courses): update CoursesPage to use server-side translations and enable dynamic title rendering

--- app/[locale]/courses/page.tsx
+++ app/[locale]/courses/page.tsx
@@ -1,11 +1,11 @@
 import { auth } from '@/lib/auth';
 import { headers } from 'next/headers';
-import { useTranslations } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 import { Suspense } from 'react';
 import CoursesList from '@/components/courses/CoursesList';
 
 export default async function CoursesPage() {
-    // const t = useTranslations('CoursesPage');
+    const t = await getTranslations('CoursesPage');
     const session = await auth.api.getSession({ headers: await headers() });
 
     return (
@@ -13,7 +13,7 @@ export default async function CoursesPage() {
             <div className="flex flex-col mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8 gap-8">
                 <div className="border-b border-gray-200 pb-5 dark:border-white/10">
                     <h3 className="text-base font-semibold text-gray-900 dark:text-white">
-                        {/* {t('title')} */} Courses
+                        {t('title')}
                     </h3>
                 </div>
                 <Suspense fallback={<div>Loading...</div>}>
