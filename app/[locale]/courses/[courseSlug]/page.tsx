cc17321dfe75186647c669d683873734b93144eb
hallsarturo <proalarturo@gmail.com>
2026-04-13 10:57:27 -0600

    feat(courses): implement Paralax component for course landing page

--- app/[locale]/courses/[courseSlug]/page.tsx
+++ app/[locale]/courses/[courseSlug]/page.tsx
@@ -1,18 +1,5 @@
+import Paralax from '@/components/courses/landing/Paralax';
+
 export default async function CourseLanding() {
-    return (
-        <div className="min-h-screen bg-white py-24 sm:py-32 dark:bg-gray-900">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl lg:mx-0">
-                    <h2 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl dark:text-white">
-                        Pseudo-Science Demarcation 1
-                    </h2>
-                    <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui
-                        irure qui lorem cupidatat commodo. Elit sunt amet fugiat
-                        veniam occaecat fugiat.
-                    </p>
-                </div>
-            </div>
-        </div>
-    );
+    return <Paralax />;
 }
---
+++ components/courses/landing/Paralax.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { motion } from 'motion/react';
+import Image from 'next/image';
+
+
+export default function Paralax() {
+    
+    return (
+        <div className="min-h-screen bg-white py-24 sm:py-32 dark:bg-gray-900">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl lg:mx-0">
+                    <h2 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl dark:text-white">
+                        Pseudo-Science Demarcation 1
+                    </h2>
+                    <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">
+                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui
+                        irure qui lorem cupidatat commodo. Elit sunt amet fugiat
+                        veniam occaecat fugiat.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
